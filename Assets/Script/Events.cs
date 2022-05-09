3661f138c2496efd9fa24862948bfc7a5e9634b7
HermanoHombre <joacoevangelisti@gmail.com>
2022-05-09 18:57:51 -0300

    V-0.1.1

---
+++ Assets/Script/Events.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Events : MonoBehaviour
+{
+    public static event Action exampleEvent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
